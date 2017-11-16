4a5e7ed8da61733e166d8c079ff6f3445633e98e
zhongjing <zhongjing@zillionfortune.com>
2017-11-16 16:49:35 +0800

    添加 java7 特性 例子

---
+++ src/main/java/com/java/study/binarySystem/Test.java
@@ -0,0 +1,31 @@
+package com.java.study.binarySystem;
+
+/**
+ * Created by zhongjing on 2016/1/27 0027.
+ *
+ * 二进制
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        //java7 特性（下划线）
+//        double height = 8_8_8_1.22;
+//        int width = 2_2_2_2;
+//        System.out.println(height + width);
+//        //10101110 转十进制
+//
+//        //计算方法：0乘以2的0次方~1乘以2的7次方，相加的和
+//        int a = 0b10101110;
+//        System.out.println(a);
+//
+//
+//        //5的二进制(0000101,前面的0省略)，9的二进制(00001001,前面的0省略)
+//
+//        System.out.println(5 & 9);
+//        System.out.println(5 | 9);
+//        System.out.println(5 ^ 9);
+
+
+    }
+
+}
